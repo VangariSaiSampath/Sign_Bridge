1ebc2a1008a2f375ba1d5f3c8b6446bbba28a626
VangariSaiSampath <saisampath786@gmail.com>
2026-03-29 15:58:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ==========================================
+# 
 # DATABASE SETUP
-# ==========================================
+# 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./signbridge.db")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
